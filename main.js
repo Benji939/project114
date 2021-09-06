8c1312e0ede94fbee54e92d1ea124d8d165a6f7c
Benji939 <76928577+Benji939@users.noreply.github.com>
2021-09-06 08:15:18 -0700

    Add files via upload

--- main.js
+++ main.js
@@ -5,12 +5,28 @@ function preload(){
 function setup(){
     canvas= createCanvas(300,300);
     canvas.center();
+    video=createCapture(VIDEO);
+    video.size(300,300);
+    video.hide();
+    poseNet=ml5.poseNet(video,modelLoaded);
+    poseNet.on('pose',gotPoses);
 }
 
 function draw(){
-
+    image(video,0,0,300,300);
 }
 function take_snapshot(){
-    save('myFilterImage,png');
+    save('MustacheImage,png');
+}
+function modelLoaded(){
+    console.log("Posenet Initialzed");
+}
+function gotPoses(results){
+    if(results.length>0){
+        console.log(results);
+        placement=results[0].pose.nose.x+20;
+        console.log("mustache x = "+placement);
+        console.log("mustache y = "+results[0].pose.nose.y);
+    }
 }
 
